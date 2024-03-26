6d0f05c280d79856aceabd813df7b044a8888c92
Matteo McCaughey SE2021 <matteo.mccaughey.2021@uni.strath.ac.uk>
2024-03-26 23:51:50 +0000

    fully working lock putted in for idletime ehehehhe

--- src/main/java/uk/ac/strath/IdleTime.java
+++ src/main/java/uk/ac/strath/IdleTime.java
@@ -4,39 +4,65 @@ import java.util.Date;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.HashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class IdleTime implements Runnable{
+public class IdleTime implements Runnable {
     private Server server;
     private Map<String, Long> lastMessages;
     private Map<String, Boolean> idleUsers;
+    private Lock lock;
 
-    public IdleTime(Server server){
+    public IdleTime(Server server) {
         this.server = server;
         lastMessages = new HashMap<>();
         idleUsers = new HashMap<>();
+        lock = new ReentrantLock();
     }
 
     @Override
-    public void run(){
-        while(server.isRunning()){
+    public void run() {
+        while (server.isRunning()) {
             long time = new Date().getTime();
 
-            Map<String, Long> copy = new HashMap<>(lastMessages);
-            Map<String, Boolean> idleCopy = new HashMap<>(idleUsers);
-
-            for (Entry<String, Long> i : copy.entrySet()) {
-                idleCopy.put(i.getKey(), time > i.getValue() + 10000);
+            Map<String, Long> copy;
+            Map<String, Boolean> idleCopy;
+            lock.lock();
+            try {
+                copy = new HashMap<>(lastMessages);
+                idleCopy = new HashMap<>(idleUsers);
+            } finally {
+                lock.unlock();
             }
-            idleUsers = idleCopy;
-        }
 
+            for (Entry<String, Long> entry : copy.entrySet()) {
+                idleCopy.put(entry.getKey(), time > entry.getValue() + 10000);
+            }
+
+            lock.lock();
+            try {
+                idleUsers = idleCopy;
+            } finally {
+                lock.unlock();
+            }
+        }
     }
 
     public void setLastMessage(String user, long time) {
-        lastMessages.put(user, time);
+        lock.lock();
+        try {
+            lastMessages.put(user, time);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public boolean isIdle(String user) {
-        return idleUsers.containsKey(user) ? idleUsers.get(user) : true;
+        lock.lock();
+        try {
+            return idleUsers.containsKey(user) ? idleUsers.get(user) : true;
+        } finally {
+            lock.unlock();
+        }
     }
 }
