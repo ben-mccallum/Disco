a9461ba77f9fb906327d8b53613a2c673e82dce5
Cameron Waddell <cameron.waddell.2021@uni.strath.ac.uk>
2024-03-26 23:54:45 +0000

    Made branch with unsafe version of IdleTime

--- src/main/java/uk/ac/strath/PollOnlineUsers.java
+++ src/main/java/uk/ac/strath/PollOnlineUsers.java
@@ -3,8 +3,8 @@ package uk.ac.strath;
 import java.util.Date;
 
 public class PollOnlineUsers implements Runnable {
-    private volatile Client client; // Adding volatile keyword for visibility
-    private volatile long lastPoll; // Adding volatile keyword for visibility
+    private Client client;
+    private long lastPoll;
 
     public PollOnlineUsers(Client client) {
         this.client = client;
@@ -16,12 +16,8 @@ public class PollOnlineUsers implements Runnable {
             long time = new Date().getTime();
 
             if (time > lastPoll + 1000) {
-                if (client != null && client.getOut() != null) { // Check for null client or output stream
-                    client.getOut().println("ONLINE");
-                    lastPoll = time;
-                } else {
-                    System.out.println("Client or output stream is null."); // Print error message
-                }
+                client.getOut().println("ONLINE");
+                lastPoll = time;
             }
         }
     }
